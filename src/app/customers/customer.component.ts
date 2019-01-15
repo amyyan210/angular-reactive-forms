b8aab93d9321829b2de75b3d3e2f938741e5e89e
ayanaway@wbhq.com <amy@yanaway.com>
2019-01-15 14:23:57 -0600

    Add subscription to react to changes instead of click events

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -57,6 +57,10 @@ export class CustomerComponent implements OnInit {
       sendCatalog: true, // true by default,
       rating: [null, ratingRange(1, 5)],
     });
+
+    this.customerForm.get('notification').valueChanges.subscribe(
+      value => this.setNotification(value)
+    );
   }
 
   save() {
